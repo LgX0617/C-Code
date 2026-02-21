c83a916d2841f6d002911bb8a4fa44c743e84cfa
LgX <2713666299@qq.com>
2026-02-21 10:00:25 +0800

    字符串函数

---
+++ Basic_Learning/test_2_9.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<string.h>
+#include<assert.h>
+
+// int my_strcmp(const char* a, const char* b)
+// {
+//     assert(a && b);
+//     while(*a && (*a == *b)){
+//         a++;
+//         b++;
+
+//     }
+//      return (*a - *b);
+//     // return *a>*b? 1:-1;
+// }
+
+// int main()
+// {
+//     // char arr1[20] = "zhangsan";
+//     // char arr2[] = "zhangsanfeng";
+//     char arr1[] = "abc";
+//     char arr2[] = "abc";
+//     //比较两个字符串是否相等
+//     int ret = my_strcmp(arr1,arr2);
+//    if(ret < 0)
+//         printf("<\n");
+//    else if(ret > 0)
+//         printf(">\n");
+//    else
+//         printf("=\n");
+
+
+//     return 0;
+
+// }
+
+int main()
+{
+    char arr1[] = "abcdef";
+    char arr2[] = "abc";
+    int ret = strncmp(arr1,arr2,4);
+    if(ret == 0)
+        printf("==\n");
+    else if(ret < 0)
+        printf("<\n");
+    else 
+        printf(">\n");
+
+        return 0;
+
+
+
+    
+}
