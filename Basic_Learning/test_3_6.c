8d1aec69d57bc755bfcb70da7b46934656f8b6c9
LgX <2713666299@qq.com>
2026-03-08 09:31:18 +0800

    联合体存在内存对齐

--- Basic_Learning/test_3_6.c
+++ Basic_Learning/test_3_6.c
@@ -39,28 +39,82 @@
 
 //联合(共用体)
 
-union Un
-{
-    char c;
-    int i;
-};
+// union Un
+// {
+//     char c;
+//     int i;
+// };
 
-struct St
-{
-    int a;
-    char c;
+// struct St
+// {
+//     int a;
+//     char c;
 
-};
-int main()
-{
-    union Un u;
-    struct St n;
-    printf("%d\n",sizeof(u));
-    printf("%d\n",sizeof(n));   
+// };
+// int main()
+// {
+//     union Un u;
+//     u.i = 0x11223344;
+//     u.c = 0x00;
+//     struct St n;
+//     printf("%d\n",sizeof(u));
+//     printf("%d\n",sizeof(n));   
 
-    printf("%p\n",&u);
-    printf("%p\n",&(u.c));
-    printf("%p\n",&(u.i));
-    return 0;
+//     printf("%p\n",&u);
+//     printf("%p\n",&(u.c));
+//     printf("%p\n",&(u.i));
+//     return 0;
 
-}
+// }
+
+
+//判断当前计算机的大小端存储
+
+// int check_sys()
+// {
+//     int a = 1;
+//     return *(char*)&a;
+// }
+
+// int check_sys()
+// {
+//     //匿名类型
+//     union 
+//     {
+//         char c;
+//         int i;
+//     }u;
+//     u.i = 1;
+//     //返回1是小端，返回0是大端
+//     return u.c;
+
+// }
+// //1
+//  int main()
+//  {
+//     // int a = 1;//0x 00 00 00 01
+//     int ret = check_sys();
+//     if(ret == 1)
+//     {
+//         printf("小端\n");
+
+//     }
+//     else
+//         printf("大端\n");
+
+//     return 0;
+
+//  }
+
+// union Un
+// {
+//     char arr[5];
+//     int a;
+// };
+
+// int main()
+// {
+//     printf("%d\n",sizeof(union Un));
+//     return 0;
+    
+// }
